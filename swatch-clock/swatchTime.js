1dc84cc17d1751a6ad228ff9cc40cfe61dc3b86f
Daniel <32464403+dkallen78@users.noreply.github.com>
2020-08-09 18:22:33 -0500

    testing fonts

--- swatch-clock/swatchTime.js
+++ swatch-clock/swatchTime.js
@@ -9,7 +9,18 @@ let refreshInterval = setInterval(function() {
   let msMinutes = (time.getUTCMinutes() * 60000) + msSeconds;
   let msTotal = ((time.getUTCHours() + 1) * 3600000) + msMinutes;
 
-  let timeString = (msTotal / 86400).toPrecision(5);
+  let rawBeats = msTotal / 86400;
+  let beats = (Math.floor(rawBeats) % 1000).toString(10).padStart(3, 0);
+  //let subBeats = ((rawBeats % 1) * 100).toPrecision(2).toString(10);
+  //let subBeats = ((rawBeats % 1).toPrecision(2) * 100).toString(10).padStart(2, 0);
+
+  //let subBeats = ((rawBeats * 100) % 100).toPrecision(2).toString(10).padStart(2, 0);
+  let subBeats = Math.floor((rawBeats * 100) % 100).toString(10).padStart(2, 0);
+  //console.clear();
+  //console.log(testBeats);
+
+
+  let timeString = beats + "." + subBeats;
 
   swatchTime.innerHTML = "@" + timeString;
 
