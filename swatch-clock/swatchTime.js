ac547fafb19d04ce608519f09e559a9920ff735e
Daniel <32464403+dkallen78@users.noreply.github.com>
2020-08-09 21:32:12 -0500

    damn fine watch Diane

--- swatch-clock/swatchTime.js
+++ swatch-clock/swatchTime.js
@@ -1,27 +1,28 @@
-let swatchTime = document.getElementById("swatchTime");
+let faces = document.getElementsByClassName("face");
 
+//
+//Refresh the time at 100Hz
 let refreshInterval = setInterval(function() {
-
 
   let time = new Date();
 
+  //
+  //Calculates the total milliseconds that have elapsed since
+  //midnight in Switzerland
   let msSeconds = (time.getUTCSeconds() * 1000) + time.getUTCMilliseconds();
   let msMinutes = (time.getUTCMinutes() * 60000) + msSeconds;
   let msTotal = ((time.getUTCHours() + 1) * 3600000) + msMinutes;
 
   let rawBeats = msTotal / 86400;
   let beats = (Math.floor(rawBeats) % 1000).toString(10).padStart(3, 0);
-  //let subBeats = ((rawBeats % 1) * 100).toPrecision(2).toString(10);
-  //let subBeats = ((rawBeats % 1).toPrecision(2) * 100).toString(10).padStart(2, 0);
-
-  //let subBeats = ((rawBeats * 100) % 100).toPrecision(2).toString(10).padStart(2, 0);
   let subBeats = Math.floor((rawBeats * 100) % 100).toString(10).padStart(2, 0);
-  //console.clear();
-  //console.log(testBeats);
+  let timeString = "@" + beats + "." + subBeats;
 
-
-  let timeString = beats + "." + subBeats;
-
-  swatchTime.innerHTML = "@" + timeString;
+  //
+  //"Writes" the individual letters of the time into
+  //separate spans
+  for (let i = 0; i < faces.length; i++) {
+    faces[i].innerHTML = timeString[i];
+  }
 
 }, 10);
