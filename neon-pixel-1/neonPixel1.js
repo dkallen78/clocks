a49ebbf337bbc6f71938498793b0f8e356660082
Daniel <32464403+dkallen78@users.noreply.github.com>
2020-08-10 14:42:14 -0500

    pixels!
    sort of

---
+++ neon-pixel-1/neonPixel1.js
@@ -0,0 +1,163 @@
+
+let shownSeconds = 0;
+let shownMinutes = 0
+let shownHours = 0;
+
+let zero = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 0, 1],
+  [1, 0, 1],
+  [1, 1, 1]
+];
+
+let one = [
+  [1, 1, 0],
+  [0, 1, 0],
+  [0, 1, 0],
+  [0, 1, 0],
+  [1, 1, 1]
+];
+
+let two = [
+  [1, 1, 1],
+  [0, 0, 1],
+  [1, 1, 1],
+  [1, 0, 0],
+  [1, 1, 1]
+];
+
+let three = [
+  [1, 1, 1],
+  [0, 0, 1],
+  [1, 1, 1],
+  [0, 0, 1],
+  [1, 1, 1]
+];
+
+let four = [
+  [1, 0, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+  [0, 0, 1],
+  [0, 0, 1]
+];
+
+let five = [
+  [1, 1, 1],
+  [1, 0, 0],
+  [1, 1, 1],
+  [0, 0, 1],
+  [1, 1, 1]
+];
+
+let six = [
+  [1, 0, 0],
+  [1, 0, 0],
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1]
+];
+
+let seven = [
+  [1, 1, 1],
+  [0, 0, 1],
+  [0, 0, 1],
+  [0, 0, 1],
+  [0, 0, 1]
+];
+
+let eight = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1]
+];
+
+let nine = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+  [0, 0, 1],
+  [0, 0, 1]
+];
+
+const digit = {
+  //----------------------------------------------------//
+  //                                                    //
+  //----------------------------------------------------//
+
+  "0": zero,
+  "1": one,
+  "2": two,
+  "3": three,
+  "4": four,
+  "5": five,
+  "6": six,
+  "7": seven,
+  "8": eight,
+  "9": nine
+}
+
+function makeDigit(target, digit) {
+
+  target.innerHTML = "";
+
+  for (let i = 0; i < digit.length; i++) {
+    for (let j = 0; j < digit[0].length; j++) {
+
+      if (digit[i][j]) {
+        let div = document.createElement("div");
+        div.style.gridRow = `${i + 1} / ${i + 2}`;
+        div.style.gridColumn = `${j + 1} / ${j + 2}`;
+        target.appendChild(div);
+      }
+
+    }
+  }
+}
+
+let hour1 = document.getElementById("hour1");
+let hour2 = document.getElementById("hour2");
+let minute1 = document.getElementById("minute1");
+let minute2 = document.getElementById("minute2");
+let second1 = document.getElementById("second1");
+let second2 = document.getElementById("second2");
+
+
+let hourNow, minuteNow, secondNow, time;
+
+makeDigit(hour1, nine);
+let refreshInterval = setInterval(function() {
+
+  time = new Date();
+  hourNow = time.getHours().toString(10).padStart(2, 0);
+  minuteNow = time.getMinutes().toString(10).padStart(2, 0);
+  secondNow = time.getSeconds().toString(10).padStart(2, 0);
+
+  if (shownHours !== hourNow) {
+
+    makeDigit(hour1, digit[hourNow[0]]);
+    makeDigit(hour2, digit[hourNow[1]]);
+
+    shownHours = hourNow;
+  }
+
+  if (shownMinutes !== minuteNow) {
+
+    makeDigit(minute1, digit[minuteNow[0]]);
+    makeDigit(minute2, digit[minuteNow[1]]);
+
+    shownMinutes = minuteNow;
+  }
+
+  if (shownSeconds !== secondNow) {
+
+    makeDigit(second1, digit[secondNow[0]]);
+    makeDigit(second2, digit[secondNow[1]]);
+
+    currentSeconds = secondNow;
+  }
+
+}, 10);
